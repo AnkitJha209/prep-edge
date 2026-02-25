e5a9180c8ef69d4958768925fc4d5099dbf9690d
Ankit Jha <professionalaj209@gmail.com>
2026-02-25 22:39:21 +0530

    added get report controller and route

--- apps/backend/src/routes/interview.routes.ts
+++ apps/backend/src/routes/interview.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { verifyCANDIDATE, verifyToken } from "../middlewares/auth.middleware";
-import { createInterview } from "../controllers/interview.controller";
+import { createInterview, getInterviewReport } from "../controllers/interview.controller";
 
 export const interviewRouter : Router = Router()
 
 interviewRouter.post('/create-interview', verifyToken, verifyCANDIDATE, createInterview)
+
+interviewRouter.get('/get-interview-report/:reportId', verifyToken, getInterviewReport)
